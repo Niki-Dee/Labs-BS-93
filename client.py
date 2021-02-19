2287bcc4fe3ce82be0493f381ff998a91bbf0983
Niki-Dee <78853196+Niki-Dee@users.noreply.github.com>
2021-02-19 11:42:39 +0200

    re

--- client.py
+++ client.py
@@ -2,60 +2,102 @@ import socket
 import json
 
 
+def decoding(data):
+    my_result = json.loads(data.decode(CODE))
+    print(my_result)
+
+
+def client_inquiry(client, client_input):
+    client.send(json.dumps(client_input).encode(CODE))
+    data = client.recv(2048)
+    decoding(data)
+
+
+
+
 HOST = "localhost"
-PORT = 1234
+PORT = 3567
 CODE = "utf-8"
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client1.connect((HOST, PORT))
+client2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client2.connect((HOST, PORT))
+client3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client3.connect((HOST, PORT))
+client4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client4.connect((HOST, PORT))
+client5 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client5.connect((HOST, PORT))
+client6 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client6.connect((HOST, PORT))
+client7 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client7.connect((HOST, PORT))
+client8 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client8.connect((HOST, PORT))
+client9 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client9.connect((HOST, PORT))
+client10 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client10.connect((HOST, PORT))
+client11 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client11.connect((HOST, PORT))
+client12 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client12.connect((HOST, PORT))
+client13 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client13.connect((HOST, PORT))
+client14 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client14.connect((HOST, PORT))
+client15 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client15.connect((HOST, PORT))
+client16 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client16.connect((HOST, PORT))
+client17 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client17.connect((HOST, PORT))
 
-client.connect((HOST, PORT))
-
-while True:
-    data = client.recv(2048)
-    print(data.decode(CODE))
-
-    client_input1 = (input("input your N:"))
-    client_input2 = (input("input your N:"))
-    client_input3 = (input("input your N:"))
-    client_input4 = (input("input your N:"))
-    client_input5 = (input("input your N:"))
-    client_input6 = (input("input your N:"))
-    client_input7 = (input("input your N:"))
-    client_input8 = (input("input your N:"))
-    client_input9 = (input("input your N:"))
-    client_input10 = (input("input your N:"))
-    client_input11 = (input("input your N:"))
-    client_input12 = (input("input your N:"))
-    client_input13 = (input("input your N:"))
-    client_input14 = (input("input your N:"))
-    client_input15 = (input("input your N:"))
-    client_input16 = (input("input your N:"))
-    client_input17 = (input("input your N:"))
 
 
-    dictionary = {
-        "Запит №1": client_input1,
-        "Запит №2": client_input2,
-        "Запит №3": client_input3,
-        "Запит №4": client_input4,
-        "Запит №5": client_input5,
-        "Запит №6": client_input6,
-        "Запит №7": client_input7,
-        "Запит №8": client_input8,
-        "Запит №9": client_input9,
-        "Запит №10": client_input10,
-        "Запит №11": client_input11,
-        "Запит №12": client_input12,
-        "Запит №13": client_input13,
-        "Запит №14": client_input14,
-        "Запит №15": client_input15,
-        "Запит №16": client_input16,
-        "Запит №17": client_input17,
-    }
+client_input1 = (input("input your N:"))
+client_input2 = (input("input your N:"))
+client_input3 = (input("input your N:"))
+client_input4 = (input("input your N:"))
+client_input5 = (input("input your N:"))
+client_input6 = (input("input your N:"))
+client_input7 = (input("input your N:"))
+client_input8 = (input("input your N:"))
+client_input9 = (input("input your N:"))
+client_input10 = (input("input your N:"))
+client_input11 = (input("input your N:"))
+client_input12 = (input("input your N:"))
+client_input13 = (input("input your N:"))
+client_input14 = (input("input your N:"))
+client_input15 = (input("input your N:"))
+client_input16 = (input("input your N:"))
+client_input17 = (input("input your N:"))
 
-    my_json = json.dumps(dictionary)
-    client.send(my_json.encode(CODE))
+client_inquiry(client1, client_input1)
+client_inquiry(client2, client_input2)
+client_inquiry(client3, client_input3)
+client_inquiry(client4, client_input4)
+client_inquiry(client5, client_input5)
+client_inquiry(client6, client_input6)
+client_inquiry(client7, client_input7)
+client_inquiry(client8, client_input8)
+client_inquiry(client9, client_input9)
+client_inquiry(client10, client_input10)
+client_inquiry(client11, client_input11)
+client_inquiry(client12, client_input12)
+client_inquiry(client13, client_input13)
+client_inquiry(client14, client_input14)
+client_inquiry(client15, client_input15)
+client_inquiry(client16, client_input16)
+client_inquiry(client17, client_input17)
+
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import socket
 import json
 from threading import Thread
+import time
 
 
-def function (N, my_dict):
+def function(N, user_socket, CODE, index):
     number = int(N)
-    for key, elem in my_dict.items():
-       if elem == N:
-            print(f'{key}: {number*number}')
+    result = f"Відповідь на запит {index}: {number*number}"
+    user_socket.send(json.dumps(result).encode(CODE))
+
 
 HOST = "localhost"
-PORT = 1234
+PORT = 3567
 CODE = "utf-8"
 
 
@@ -19,70 +20,19 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server.bind((HOST, PORT))
 
-server.listen(5)
+server.listen()
 print("Server is listening !")
-
+index = 1
 while True:
     user_socket, address = server.accept()
-    print(f"User {address} connected !")
 
-    user_socket.send("You are connected !".encode(CODE))
     data = user_socket.recv(2048)
-    my_json = data.decode(CODE)
-    my_dict = json.loads(my_json)
+    number = json.loads(data.decode(CODE))
 
-    t1 = Thread(target=function, args=(my_dict["Запит №1"], my_dict) )
-    t2 = Thread(target=function, args=(my_dict["Запит №2"], my_dict))
-    t3 = Thread(target=function, args=(my_dict["Запит №3"], my_dict))
-    t4 = Thread(target=function, args=(my_dict["Запит №4"], my_dict))
-    t5 = Thread(target=function, args=(my_dict["Запит №5"], my_dict))
-    t6 = Thread(target=function, args=(my_dict["Запит №6"], my_dict))
-    t7 = Thread(target=function, args=(my_dict["Запит №7"], my_dict))
-    t8 = Thread(target=function, args=(my_dict["Запит №8"], my_dict))
-    t9 = Thread(target=function, args=(my_dict["Запит №9"], my_dict))
-    t10 = Thread(target=function, args=(my_dict["Запит №10"], my_dict))
-    t11 = Thread(target=function, args=(my_dict["Запит №11"], my_dict))
-    t12 = Thread(target=function, args=(my_dict["Запит №12"], my_dict))
-    t13 = Thread(target=function, args=(my_dict["Запит №13"], my_dict))
-    t14 = Thread(target=function, args=(my_dict["Запит №14"], my_dict))
-    t15 = Thread(target=function, args=(my_dict["Запит №15"], my_dict))
-    t16 = Thread(target=function, args=(my_dict["Запит №16"], my_dict))
-    t17 = Thread(target=function, args=(my_dict["Запит №17"], my_dict))
+    t1 = Thread(target=function, args=(number, user_socket, CODE,index))
     t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
-    t5.start()
-    t6.start()
-    t7.start()
-    t8.start()
-    t9.start()
-    t10.start()
-    t11.start()
-    t12.start()
-    t13.start()
-    t14.start()
-    t15.start()
-    t16.start()
-    t17.start()
-
-    t1.join()
-    t2.join()
-    t3.join()
-    t4.join()
-    t5.join()
-    t6.join()
-    t7.join()
-    t8.join()
-    t9.join()
-    t10.join()
-    t11.join()
-    t12.join()
-    t13.join()
-    t14.join()
-    t15.join()
-    t16.join()
-    t17.join()
+   # time.sleep(1)
+    index += 1
 
 
 
